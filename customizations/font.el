f6e83906948023fe8f36c341b8361deebb9701c5
李富荣 <leefurong@gmail.com>
2019-08-16 03:20:11 +0800

    initial commit. a lot of stuff

---
+++ customizations/font.el
@@ -0,0 +1,7 @@
+(set-face-attribute
+'default nil :font "黑体 16")
+;; Chinese Font
+;; (dolist (charset '(kana han symbol cjk-misc bopomofo))
+  ;; (set-fontset-font (frame-parameter nil 'font)
+                    ;; charset
+                    ;; (font-spec :family "WenQuanYi Micro Hei Mono" :size 16)))
